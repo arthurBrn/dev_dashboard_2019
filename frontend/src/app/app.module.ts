b0df9b805cd0ee955372d73b1eff45de6207f854
arthur baron <baronarthur3@gmail.com>
2020-04-11 19:47:03 +0200

    [EDIT] mounted volume to reload code after each modification on the front.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingArrayOfComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    routingArrayOfComponent,
   ],
   imports: [
     BrowserModule,
