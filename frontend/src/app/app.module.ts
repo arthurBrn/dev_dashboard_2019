a5b17318b7073038fc47e8d645bc44340f80eac1
arthur baron <baronarthur3@gmail.com>
2020-04-11 20:48:42 +0200

    [ADD] register component added.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FormsModule} from "@angular/forms";
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     routingArrayOfComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
