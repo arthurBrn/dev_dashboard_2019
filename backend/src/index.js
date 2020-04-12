fa20c3e892a6c5ae797a788ae13935eab9696af6
arthur baron <baronarthur3@gmail.com>
2020-04-12 23:31:54 +0200

    [EDIT] nodemon added for nodejs to aknowledge code changes.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,32 @@
 const express = require('express')
+// const mysql = require('mysql');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
+const app = express();
+/*app.use(
+    bodyParser.urlEncoded({
+      extended: true
+    })
+)
+app.use(cors());*/
 
-const app = express()
+
 
 app.get('/', (req, res) => {
   res.send('hello world');
 });
+
+/*const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'monty',
+  password: 'monty',
+  database: 'dashboard',
+});
+connection.connect((err) => {
+  if (err) {
+    console.log('CONNECTION FAILED');
+  }
+  console.log('connected to db');
+});*/
 
 app.listen(8080);
