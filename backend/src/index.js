849358810e82f6b140d94010b9e5ea8506871bdd
arthur baron <baronarthur3@gmail.com>
2020-04-16 22:07:56 +0200

    [FIX] route / error fixed.

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,20 +7,24 @@ const db = require('./db');
 const app = express();
 const jwt = require('jsonwebtoken');
 
-app.use(express.json);
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('hello world');
 });
+/*
 app.get('/users/all', (req, res) => {
     db.query("SELECT * FROM users;", (err, rows) => {
         res.send(rows);
         db.end;
     })
 });
-
+*/
 /*
 app.post('/login', (req, res) => {
 })
@@ -28,11 +32,13 @@ app.post('/login', (req, res) => {
 
 app.listen(8080, () => {
     console.log('SERVER LISTENING ON PORT 8080');
+    /*
     console.log(
         db.query("SELECT * from users;", (err, rows) => {
             console.log(rows);
             db.end;
         })
     )
+    */
     console.log(listEndpoints(app));
 });
