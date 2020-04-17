3f4e281879db4a301d0dd3ab829ab21788b02e70
Maxime Bouchet <maxime.bouchet@epitech.eu>
2020-04-17 20:12:30 +0200

    grid list for services

--- frontend/src/app/display-services/display-services.component.ts
+++ frontend/src/app/display-services/display-services.component.ts
@@ -1,11 +1,82 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-display-services',
   templateUrl: './display-services.component.html',
   styleUrls: ['./display-services.component.css']
 })
 export class DisplayServicesComponent implements OnInit {
+
+  image = "https://placetech.net/wp-content/uploads/2018/04/cryptocurrency-3085139_1280.jpg";
+  title = "Cryptocurrencies";
+
+  services = [
+    {
+      image: "https://placetech.net/wp-content/uploads/2018/04/cryptocurrency-3085139_1280.jpg",
+      title: "Cryptocurrencies"
+    },
+    {
+      image: "https://placetech.net/wp-content/uploads/2018/04/cryptocurrency-3085139_1280.jpg",
+      title: "Cryptocurrencies"
+    },
+    {
+      image: "https://placetech.net/wp-content/uploads/2018/04/cryptocurrency-3085139_1280.jpg",
+      title: "Cryptocurrencies"
+    },
+    {
+      image: "https://placetech.net/wp-content/uploads/2018/04/cryptocurrency-3085139_1280.jpg",
+      title: "Cryptocurrencies"
+    },
+    {
+      image: "https://placetech.net/wp-content/uploads/2018/04/cryptocurrency-3085139_1280.jpg",
+      title: "Cryptocurrencies"
+    },
+    {
+      image: "https://placetech.net/wp-content/uploads/2018/04/cryptocurrency-3085139_1280.jpg",
+      title: "Cryptocurrencies"
+    },
+    {
+      image: "https://placetech.net/wp-content/uploads/2018/04/cryptocurrency-3085139_1280.jpg",
+      title: "Cryptocurrencies"
+    },
+    {
+      image: "https://placetech.net/wp-content/uploads/2018/04/cryptocurrency-3085139_1280.jpg",
+      title: "Cryptocurrencies"
+    },
+    {
+      image: "https://placetech.net/wp-content/uploads/2018/04/cryptocurrency-3085139_1280.jpg",
+      title: "Cryptocurrencies"
+    },
+    {
+      image: "https://placetech.net/wp-content/uploads/2018/04/cryptocurrency-3085139_1280.jpg",
+      title: "Cryptocurrencies"
+    },
+    {
+      image: "https://placetech.net/wp-content/uploads/2018/04/cryptocurrency-3085139_1280.jpg",
+      title: "Cryptocurrencies"
+    },
+    {
+      image: "https://placetech.net/wp-content/uploads/2018/04/cryptocurrency-3085139_1280.jpg",
+      title: "Cryptocurrencies"
+    },
+    {
+      image: "https://placetech.net/wp-content/uploads/2018/04/cryptocurrency-3085139_1280.jpg",
+      title: "Cryptocurrencies"
+    },
+    {
+      image: "https://placetech.net/wp-content/uploads/2018/04/cryptocurrency-3085139_1280.jpg",
+      title: "Cryptocurrencies"
+    },
+    {
+      image: "https://placetech.net/wp-content/uploads/2018/04/cryptocurrency-3085139_1280.jpg",
+      title: "Cryptocurrencies"
+    },
+    {
+      image: "https://placetech.net/wp-content/uploads/2018/04/cryptocurrency-3085139_1280.jpg",
+      title: "Cryptocurrencies"
+    },
+  ];
 
   constructor() { }
 
---
+++ frontend/src/app/display-services/service-card/service-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-service-card',
+  templateUrl: './service-card.component.html',
+  styleUrls: ['./service-card.component.css']
+})
+export class ServiceCardComponent implements OnInit {
+
+  
+
+  @Input() image;
+  @Input() title;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
