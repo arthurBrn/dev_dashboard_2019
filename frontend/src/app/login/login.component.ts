8d97b2f223046c41c5c6f8f59b77313bda2178dd
arthur baron <baronarthur3@gmail.com>
2020-04-17 19:57:59 +0200

    [EDIT] done.

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, EventEmitter, OnInit, Output, TemplateRef } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import {ApiService} from "../service/api.service";
+import { ApiService } from '../service/api.service';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +14,6 @@ export class LoginComponent implements OnInit {
 
   @Output() validCrendentialEvent = new EventEmitter();
   modalRef: BsModalRef;
-  valideCredentialValue: boolean;
   forgottenPassword = false;
   mailAddressForForgottenPassword: string;
   email: string = "";
@@ -24,6 +25,8 @@ export class LoginComponent implements OnInit {
     private _toastr: ToastrService,
     private _modalService: BsModalService,
     private _apiService: ApiService,
+    private _location: Location,
+    private _router: Router,
   ) { }
 
   ngOnInit(): void {
@@ -31,23 +34,18 @@ export class LoginComponent implements OnInit {
 
   onLogin() {
     if (this.email && this.password) {
-      this._apiService.getMail(this.email).subscribe((data) => {
+      this._apiService.login(this.email, this.password).subscribe((data) => {
+        console.log('we enter here ');
         var parsedData = data as any;
-        this.doesEmailExistInDatabase = parsedData.code === 200 ? true : false;
+        if (parsedData.code === 200) {
+          localStorage.setItem('jwtToken', parsedData.accessToken);
+          console.log(localStorage.getItem('jwtToken'));
+          this._location.replaceState('/');
+          // this._router.navigate('/');
+        } else {
+          this._toastr.warning(parsedData.success);
+        }
       });
-      if (true === this.doesEmailExistInDatabase) {
-        this._apiService.login(this.email, this.password).subscribe((data) => {
-          console.log('we enter here ');
-          var parsedData = data as any;
-          if (parsedData.code === 200) {
-            localStorage.setItem('jwtToken', parsedData.accessToken);
-          } else {
-            this._toastr.warning(parsedData.success);
-          }
-        });
-      } else {
-        this._toastr.warning('This email is not registered');
-      }
     } else {
       this._toastr.warning('All fields must be filled.');
     }
