76bed321adf35678580e47fb51b2e58b61f14d86
arthur baron <baronarthur3@gmail.com>
2020-04-17 11:40:39 +0200

    [EDIT] route register working.

--- backend/user/routes/user.js
+++ backend/user/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pool = require('../src/db');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 router.use(bodyParser.json());
@@ -17,11 +18,41 @@ router.get('/', (req, res) => {
   });
 });
 
+router.post('/mail', (req, res) => {
+  pool.getConnection().then((conn) => {
+    conn.query(
+        'SELECT * FROM users WHERE mail = ? ;',
+        [req.body.mail]
+    ).then((result) => {
+      res.status(200).json(result)
+    }).catch((err) => {
+      console.log(err);
+    })
+  }).catch((err) => {
+    console.log(err);
+  })
+})
+
 router.post('/login', (req, res) => {
   pool.getConnection().then((conn) => {
     conn.query(
         'SELECT * FROM users WHERE mail = ? AND password = ? ;',
         [req.body.mail, req.body.password],
+    ).then((result) => {
+      res.status(200).json(result);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
+});
+
+router.post('/register', (req, res) => {
+  pool.getConnection().then((conn) => {
+    conn.query(
+        'INSERT INTO users (first_name, last_name, mail, password) VALUES (?,?,?,?);',
+        [req.body.firstName, req.body.lastName, req.body.mail, req.body.password]
     ).then((result) => {
       res.status(200).json(result);
     }).catch((err) => {
