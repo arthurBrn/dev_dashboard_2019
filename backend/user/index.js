8fb11b60ff4e034adf0fccea9cca112a2ff8c48f
arthur baron <baronarthur3@gmail.com>
2020-04-17 15:24:34 +0200

    [EDIT] JWT token generate successfully. Middleware ongoing to check the user's authorizations.

--- backend/user/index.js
+++ backend/user/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config({
-  path: path.resolve(__dirname, '.env'),
+    path: path.resolve(__dirname, '.env'),
 });
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -12,20 +12,23 @@ const app = express();
 app.use(cors());
 app.use(
     bodyParser.urlencoded({
-      extended: true,
+        extended: true,
     }),
 )
 
 app.use('/user', user);
 
 app.get('/', (req, res) => {
-  console.log('here and there');
-  console.log(pool.host);
-  res.send('Connecté au user service');
+    console.log('here and there');
+    console.log(pool.host);
+    res.send('Connecté au user service');
 });
 
 app.listen({
-  port: process.env.PORT,
+    port: process.env.PORT,
 }, () => {
-  console.log(`🚀 Server ready at http://localhost:${process.env.PORT}`);
+    console.log(`🚀 Server ready at http://localhost:${process.env.PORT}`);
 });
+
+
+
--- backend/user/routes/user.js
+++ backend/user/routes/user.js
@@ -4,6 +4,7 @@ const pool = require('../src/db');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 router.use(bodyParser.json());
+const jwt = require('jsonwebtoken');
 
 router.get('/', (req, res) => {
   pool.getConnection().then((conn) => {
@@ -32,29 +33,120 @@ router.post('/mail', (req, res) => {
   })
 })
 
+
+/**
+ * @param req
+ * @param res
+ * @param next
+ * @returns {*}
+ * Used as a midleware
+ * Verify token send in the request
+ * Add the user info to the request if token is valid
+ */
+function authenticateToken(req, res, next) {
+  // Recover authentication token in the header
+  const authHeader = req.headers['authorization'];
+  // Saying : If we have a authHeader then return the token, that we take from the split
+  const token = authHeader && authHeader.split(' ')[1];
+  if (null === token) return res.status(401).send('You need to send a token with your request');
+
+  jwt.verify(token, process.env.SECRET_TOKEN, (err, ourUser) => {
+    if (err) return res.status(403).send('Token no longer valid');
+    // If we arrive here, our token is valid, we want to continue our operations
+    req.user = ourUser;
+    next();
+  });
+}
+
+
+router.post('/login/jwt', authenticateToken, (req, res) => {
+  pool.getConnection().then((conn) => {
+    conn.query(
+        'SELECT * FROM users WHERE mail = ? ;',
+        [req.body.mail]
+    ).then((result) => {
+      if (result) {
+        bcrypt.compare(req.body.password, result[0].password, (err, respwd) => {
+          if (respwd) {
+            const user = {
+              id: result[0].id,
+              firstName: result[0].firstName,
+              lastName: result[0].lastName,
+              mail: result[0].mail,
+              password: result[0].password
+            }
+            const accessToken = jwt.sign(user, process.env.SECRET_TOKEN);
+            res.json({
+              code: 200,
+              success: 'login successfull',
+              accessToken: accessToken
+            });
+          } else {
+            res.json({
+              code: '404',
+              success: 'Login failed. Wrong password.'
+            })
+          }
+        });
+      } else {
+        res.json({
+          code: 404,
+          success: 'No address mail match this one in the database.'
+        });
+      }
+    }).catch((err) => {
+      console.log(err);
+    });
+  }).catch((err) => {
+    console.log(err);
+  })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 router.post('/login', (req, res) => {
   pool.getConnection().then((conn) => {
     conn.query(
         'SELECT * FROM users WHERE mail = ?;',
-        [req.body.mail, req.body.password],
+        [req.body.mail],
     ).then((result) => {
-      if (false === result.isNull) {
-        bcrypt.compare(req.body.password, result.password, (err, pwdResult) => {
+      if (result) {
+        bcrypt.compare(req.body.password, result[0].password, (err, pwdResult) => {
           if (pwdResult) {
             res.json({
               code:200,
               success: 'Login success',
-              userId: pwdResult.id
+              userId: result[0].id
             })
           } else {
             res.json({
-              code:204,
+              code: 404,
               success: 'Email / password not matching'
             })
           }
         })
+      } else {
+        res.status(404).send('No mail matching the one entered');
       }
-      res.status(200).json(result);
     }).catch((err) => {
       console.log(err);
     });
