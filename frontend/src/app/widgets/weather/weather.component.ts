839407c45e93c39af99cbf20f43ffa84c7ed3918
arthur baron <baronarthur3@gmail.com>
2020-04-19 14:53:10 +0200

    [EDIT] button to add widget done. Modal popping up done. Collect data enter by user about widget done.

--- frontend/src/app/widgets/weather/weather.component.ts
+++ frontend/src/app/widgets/weather/weather.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef  } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-weather',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherComponent implements OnInit {
 
-  constructor() { }
+  modalRef: BsModalRef;
+  widgetName: string;
+  widgetCityName: string;
+  widgetTimePeriod: string;
+
+  constructor(
+    private _toastr: ToastrService,
+    private _modalService: BsModalService,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onNewWidgetClick(template: TemplateRef<any>) {
+    let plusIcon = document.getElementById('weather-plus-icon');
+    plusIcon.classList.add('fa-spin');
+    console.log(plusIcon);
+    this.modalRef = this._modalService.show(template);
+  }
+
+  onValidateNewWidget() {
+    if (this.widgetName && this.widgetCityName && this.widgetTimePeriod) {
+      this._modalService.hide(1);
+      console.log(this.widgetName);
+      console.log(this.widgetCityName);
+      console.log(this.widgetTimePeriod);
+    } else {
+      this._toastr.warning('All fields must be filled.');
+    }
   }
 
 }
