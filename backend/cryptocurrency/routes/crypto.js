62582403d370bbe547dda41287d2d787390520b5
arthur baron <baronarthur3@gmail.com>
2020-04-22 21:25:32 +0200

    [EDIT] autenticateMethod copied inside weather and crypto service.

--- backend/cryptocurrency/routes/crypto.js
+++ backend/cryptocurrency/routes/crypto.js
@@ -5,6 +5,29 @@ const request = require('request');
 const router = express.Router();
 router.use(bodyParser.json());
 
+/**
+ * @param req
+ * @param res
+ * @param next
+ * @returns {*}
+ * Used as a midleware
+ * Verify token send in the request
+ * Add the user info to the request if token is valid
+ */
+function authenticateToken(req, res, next) {
+  // Recover authentication token in the header
+  const authHeader = req.headers['authorization'];
+  // Saying : If we have a authHeader then return the token, that we take from the split
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token === null) return res.status(401).send('You need to send a token with your request');
+
+  jwt.verify(token, process.env.SECRET_TOKEN, (err, ourUser) => {
+    if (err) return res.status(403).send('Token no longer valid');
+    // If we arrive here, our token is valid, we want to continue our operations
+    req.user = ourUser;
+    next();
+  });
+}
 
 router.get('/graph', (req, res) => {
   const requestUrl = `https://api.coincap.io/v2/assets/${req.query.crypto}/history?interval=d1&start=${req.query.start}&end=${req.query.end}`;
